0171204743e58e17fc5eb23ffdc286e371b57d82
Chen Wilson <chenlwilson@gmail.com>
2018-12-17 17:43:46 -0800

    added notes in controllers

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -1,17 +1,39 @@
+/*
+TODO: transfer server code to each get/post functions
+
+https://expressjs.com/en/4x/api.html#router.METHOD
+router.get('/', function(req, res){
+  res.send('hello world');
+});
+used in routes.js:
+router.get('/messages', controller.messages.get);
+*/
+
 var models = require('../models');
 
 module.exports = {
+  // a function which handles a get request for all messages
   messages: {
     get: function (req, res) {
-      models.messages.get()
-    }, // a function which handles a get request for all messages
-    post: function (req, res) {} // a function which handles posting a message to the database
+      models.messages.get();
+    },
+
+    // a function which handles posting a message to the database
+    post: function (req, res) {
+      models.messages.post();
+    }
   },
 
   users: {
-    // Ditto as above
-    get: function (req, res) {},
-    post: function (req, res) {}
+    // a function which handles a get request for all users
+    get: function (req, res) {
+      models.users.get();
+    },
+
+    // a function which handles posting a user to the database
+    post: function (req, res) {
+      models.users.post();
+    }
   }
 };
 
